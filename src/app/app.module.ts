2550595527c21a02f267ab2a2e81a0c0eb0b111b
Argueta <argueta03@hotmail.com>
2023-07-19 13:03:19 +0200

    Subida del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { AddContactComponent } from './components/schedule/add-contact/add-contact.component';
+import { ListContactsComponent } from './components/schedule/list-contacts/list-contacts.component';
+import { FormsModule } from '@angular/forms';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    AddContactComponent,
+    ListContactsComponent,
+    CapitalizePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/schedule/add-contact/add-contact.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Contact } from 'src/app/interfaces/Contact';
+import { ContactService } from 'src/app/services/contact.service';
+
+@Component({
+  selector: 'app-add-contact',
+  templateUrl: './add-contact.component.html',
+  styleUrls: ['./add-contact.component.css']
+})
+export class AddContactComponent implements OnInit{
+
+  public contact:Contact;
+
+  constructor(private contactService:ContactService,
+    private route:Router){
+    this.contact={
+      'name': '',
+      'surname': '',
+      'description': '',
+      'company': '',
+      'sex': 'hombre',
+      'telephone': '',
+      'email': ''
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  addContact(){
+    this.contactService.addContact(this.contact);
+    this.route.navigate(['/list-contact'])
+  }
+
+}
